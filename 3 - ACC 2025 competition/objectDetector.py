6bc57ed1d03d89c68c784f0bf884584ac2d34107
ZachCopenhaver <copenhzs@gmail.com>
2024-10-04 21:29:43 -0400

    10-6. Plotting data.

--- 3 - ACC 2025 competition/objectDetector.py
+++ 3 - ACC 2025 competition/objectDetector.py
@@ -4,6 +4,7 @@ import struct
 import numpy as np 
 import cv2
 import math
+from matplotlib import pyplot as plt
 
 # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 ## Timing Parameters and methods 
@@ -23,6 +24,11 @@ imageWidth = 640
 imageHeight = 480
 croppedImageHeight = int(imageHeight/2)
 #cameraID = '3'
+#grayTimeArr = np.zeros([1,1000], dtype=float)
+#hsvTimeArr = np.zeros([1,1000], dtype=float)
+grayTimeArr = []
+hsvTimeArr = []
+frames = []
 
 # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 ## Initialize the CSI cameras
@@ -66,10 +72,6 @@ def detectHSV(image, color):
     # can we make this function detect multiple colors per call?
     mask = cv2.inRange(hsv, lower, upper)
     
-    # Filter out unnecessary imaging noise occuring from varying objects in enviroment
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
-    
     _,contours,_ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
     # Draw bounding boxes around detected green objects
@@ -93,24 +95,24 @@ def detectGrayscale(image):
     # simple threshold
     # everything with a value above 100 becomes 255 (white)
 
-    # Gaussian blur to reduce noise -- smoothes out grayscale fcn prior to threshold can change sizes dependent on needs
-    # blurredImage = cv2.GaussianBlur(grayImage, (5, 5), 0)
+    # Gaussian blur to reduce noise -- smoothes out grayscale fcn prior to threshold; can change sizes dependent on needs
+    blurredImage = cv2.GaussianBlur(grayImage, (5, 5), 0)
 
-    ret, binaryImage = cv2.threshold(grayImage, 100, 255, cv2.THRESH_BINARY) # blurredImage if using 
+    ret, binaryImage = cv2.threshold(blurredImage, 100, 255, cv2.THRESH_BINARY) # blurredImage if using 
     # adaptive threshold; should help with lighting
-    # binaryImage = cv2.adaptiveThreshold(grayImage, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 211, 30) 
+    # binaryImage = cv2.adaptiveThreshold(blurredImage, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 71, 20) 
     # Otsu's method could also be helpful helps with varying illumniation levels of colors
-    # Otsu's thresholding -- > ret, binaryImage = cv2.threshold(blurredImage, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    #ret, binaryImage = cv2.threshold(blurredImage, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     
     # Morphological operations to remove noise
-    #kernel = np.ones((5, 5), np.uint8) # kernel can change sizes, larger remove more noise but costwrothy and chain effect
+    kernel = np.ones((5, 5), np.uint8) # kernel can change sizes, larger remove more noise but costwrothy and chain effect
 
     # lose small holes in the foreground objects and connect nearby objects
-    #morphedImage = cv2.morphologyEx(binaryImage, cv2.MORPH_CLOSE, kernel)
+    morphedImage = cv2.morphologyEx(binaryImage, cv2.MORPH_CLOSE, kernel)
     # removes small noise points by eroding and then dilating the image
-    #morphedImage = cv2.morphologyEx(morphedImage, cv2.MORPH_OPEN, kernel)
+    morphedImage = cv2.morphologyEx(morphedImage, cv2.MORPH_OPEN, kernel)
 
-    _,contours,_ = cv2.findContours(binaryImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # morphed Image if using morph ops
+    _,contours,_ = cv2.findContours(morphedImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # morphed Image if using morph ops
 
     # draw contours
     #Fl - https://www.labelvisor.com/challenges-and-solutions-in-implementing-tight-bounding-boxes/ introduce convex hull bounding
@@ -120,7 +122,7 @@ def detectGrayscale(image):
             """x1,y1,w,h = cv2.boundingRect(contour)
             cv2.rectangle(contourImage, (x1, y1), (x1+w, y1+h), (0,0,255), 2)"""
 
-            # espsilon param to adjust/control estimate, work to find diff values if needed give 0.01 from overflow
+            """# espsilon param to adjust/control estimate, work to find diff values if needed give 0.01 from overflow
             epsilonParam = 0.01 * cv2.arcLength(contour, True)
             # estimation of contour to further simplify and reduce needed points in RBB 
             estimate = cv2.approxPolyDP(contour, epsilonParam, True)
@@ -137,9 +139,29 @@ def detectGrayscale(image):
                 points = np.int0(points)
 
                 # draw onto original image
-                cv2.drawContours(imageContours, [points], 0, (0, 255, 0), 2)
+                cv2.drawContours(imageContours, [points], 0, (0, 255, 0), 2)"""
+
+            area = cv2.minAreaRect(contour)
+            points = cv2.boxPoints(area)
+            points = np.int0(points)
+            # draw onto original image
+            cv2.drawContours(imageContours, [points], 0, (0, 255, 0), 2)
 
     return imageContours
+
+# function to convert an image to grayscale
+def toGray(image, kernel_size=5):
+    # converts a copy of the original image to grayscale
+    image2 = image.copy()
+    # convert bgr image to grayscale
+    grayImage = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+    # Gaussian blur to reduce noise -- smoothes out grayscale fcn prior to threshold can change sizes dependent on needs
+    blurredImage = cv2.GaussianBlur(grayImage, (kernel_size, kernel_size), 0)
+    return blurredImage
+
+def cannyLines(image, low_threshold, high_threshold):
+    grayImage = toGray(image, 5)
+    edges = cv2.Canny(grayImage, low_threshold, high_threshold)
 
 # function to format how all four cameras are displayed at once
 # each camera should not interfere with the others; this function merely organizes them
@@ -164,6 +186,14 @@ def combineFeeds(leftCam, backCam, rightCam, frontCam):
     
     return allCams
 
+def findLines(image):
+    image2 = image.copy()
+    grayImage = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+    lsd = cv2.createLineSegmentDetector(0)
+    lines = lsd.detect(grayImage)[0]
+    drawn_img = lsd.drawSegments(image2, lines)
+    return drawn_img
+
 #Main Loop
 try:
     while True:
@@ -187,15 +217,25 @@ try:
         front = frontCam.image_data[croppedImageHeight:480, :].copy()
 
         # detects objects based on brightness in a grayscale format
+        #"""
+        grayAllTimeStart = time.time()
         grayscaleObjects = combineFeeds(detectGrayscale(left),
                                         detectGrayscale(back),
                                         detectGrayscale(right),
                                         detectGrayscale(front))
+        grayAllTimeEnd = time.time()
+        #"""
+        grayAllTime = grayAllTimeEnd - grayAllTimeStart
+        #grayTimeArr[0,int(counter)] = grayAllTime
+        grayTimeArr.append(grayAllTime)
+        frames.append(counter)
         
         # detect objects based on color in HSV format
         # detects white and yellow and shows on the same image
         # needs x function calls per cam to detect x number of colors
-        """leftHSV = detectHSV(left, 'white')
+        #"""
+        hsvAllTimeStart = time.time()
+        leftHSV = detectHSV(left, 'white')
         leftHSV = detectHSV(left, 'yellow')
         backHSV = detectHSV(back, 'white')
         backHSV = detectHSV(back, 'yellow')
@@ -203,7 +243,16 @@ try:
         rightHSV = detectHSV(right, 'yellow')
         frontHSV = detectHSV(front, 'white')
         frontHSV = detectHSV(front, 'yellow')
-        hsvObjects = combineFeeds(leftHSV, backHSV, rightHSV, frontHSV)"""
+        hsvObjects = combineFeeds(leftHSV, backHSV, rightHSV, frontHSV)
+        hsvAllTimeEnd = time.time()
+        hsvAllTime = hsvAllTimeEnd - hsvAllTimeStart
+        #hsvTimeArr[0,int(counter)] = hsvAllTime
+        hsvTimeArr.append(hsvAllTime)
+        #"""
+
+        # test only the front cam
+        #grayscaleObjects = detectGrayscale(front)
+        #hsvObjects = detectHSV(front, 'white')
 
         # End timing this iteration
         end = time.time()
@@ -214,14 +263,14 @@ try:
         
         # Display the four images
         # HSV
-        #cv2.imshow('HSV Objects', hsvObjects)
+        cv2.imshow('HSV Objects', hsvObjects)
         # Grayscale
         cv2.imshow('Grayscale Objects', grayscaleObjects)
-        
+
         # Pause/sleep for sleepTime in milliseconds
         # show only every 20th frame on VNC
-        if counter % 20 == 0:
-            msSleepTime = int(20000*sleepTime)
+        if counter % 10 == 0:
+            msSleepTime = int(10000*sleepTime)
             if msSleepTime <= 0:
                 msSleepTime = 1 # this check prevents an indefinite sleep as cv2.waitKey waits indefinitely if input is 0
             cv2.waitKey(msSleepTime)
@@ -235,4 +284,13 @@ finally:
     backCam.terminate()
     rightCam.terminate()
     frontCam.terminate()
+
+    """plt.plot(frames,grayTimeArr,'o',color='r')
+    plt.plot(frames,hsvTimeArr,'o',color='b')
+    plt.title('Computation time IP for each frame')
+    plt.xlabel('Frame')
+    plt.ylabel('Computation Time')
+    plt.legend(['Gray Time', 'HSV Time'])
+    plt.savefig("IP_runtime_10-6-24.jpg")
+    plt.show()"""
 # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
